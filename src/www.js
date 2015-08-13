6d8c449e6387751aa6af95099a211c7507df9367
Soheil Rashidi <contact@soheilrashidi.com>
2015-08-14 01:35:00 +0430

    Moved port setting to config. Closes #1

--- src/www.js
+++ src/www.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 var debug = require('debug')('odin-api');
+var config = require('./config');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(config.get('port'), function() {
   debug('Odin API listening on port ' + server.address().port);
 });
