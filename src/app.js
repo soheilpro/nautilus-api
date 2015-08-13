21d904691b6e65d15bbcf8d206c9c8f91db744e6
Soheil Rashidi <contact@soheilrashidi.com>
2015-08-13 12:10:15 +0430

    Added CORS support.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var debug = require('debug')('odin-api');
 
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', require('./routes/api'));
 
