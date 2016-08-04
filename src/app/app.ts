5113f86052d2a93ed8088d7de2816fa614fb6f5c
Soheil Rashidi <contact@soheilrashidi.com>
2016-08-04 23:05:03 +0430

    Removed TypeScript references.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,3 @@
-/// <reference path="./typings/index.d.ts" />
-
 import { SessionRepository } from './repositories/session';
 
 var express = require('express');
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,3 @@
-/// <reference path="./typings/index.d.ts" />
-
 var path = require('path');
 var nconf = require('nconf');
 
