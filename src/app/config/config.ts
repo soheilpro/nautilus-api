1989571d88de8230c61d1ff45375958f1e03cef3
Soheil Rashidi <contact@soheilrashidi.com>
2017-08-29 13:18:07 +0430

    config

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -6,12 +6,12 @@ const configuration = nconf
   .env()
   .file({ file: path.join(__dirname, '../../../config/app.json') })
   .defaults({
-    'NAUTILUS_API_PORT': 3000,
+    'NAUTILUS_API_PORT': '3000',
     'NAUTILUS_API_DB_ADDRESS': 'mongodb://localhost:27017/nautilus',
   });
 
 export const config: IConfig = {
-  port: configuration.get('NAUTILUS_API_PORT'),
+  port: Number(configuration.get('NAUTILUS_API_PORT')),
   db: {
     address: configuration.get('NAUTILUS_API_DB_ADDRESS'),
   },
