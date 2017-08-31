0236b9f90ac5eee7d9bd264496a6d4a30bc13a69
Soheil Rashidi <contact@soheilrashidi.com>
2017-09-01 00:38:37 +0430

    init

--- src/app/schema/v1.ts
+++ src/app/schema/v1.ts
@@ -6,28 +6,106 @@ export class v1 implements ISchema {
   readonly version = 1;
 
   async apply(db: IDB) {
-    const versionCounter: IDocument = {
+    await db.insert('counters', {
       'name': '*.meta.version',
       'value': 0,
-    };
+    });
 
-    await db.insert('counters', versionCounter);
-
-    let adminUser: IDocument = {
+    const adminUser: IDocument = await db.insertManaged('users', {
       'name': 'Admin',
       'username': 'admin',
       'passwordHash': '$2a$10$WlTsJ5UA3uC0mr6k4v9qN.uzXVRgGG08gNAqGGZctK7ioaIGoHPeu',
-    };
+    });
 
-    adminUser = await db.insertManaged('users', adminUser);
-
-    const adminUserRole = {
+    await db.insertManaged('userRoles', {
       'user': {
         '_id': adminUser._id,
       },
       'name': 'admin',
-    };
+    });
 
-    await db.insertManaged('user_roles', adminUserRole);
+    await db.insertManaged('itemTypes', {
+      'key': 'task',
+      'itemKind': 'issue',
+      'title': 'Task',
+      'order': 1,
+    });
+
+    await db.insertManaged('itemTypes', {
+      'key': 'bug',
+      'itemKind': 'issue',
+      'title': 'Bug',
+      'order': 2,
+    });
+
+    await db.insertManaged('itemTypes', {
+      'key': 'feature',
+      'itemKind': 'issue',
+      'title': 'Feature',
+      'order': 3,
+    });
+
+    await db.insertManaged('itemTypes', {
+      'key': 'enhancement',
+      'itemKind': 'issue',
+      'title': 'Enhancement',
+      'order': 4,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'planned',
+      'itemKind': 'milestone',
+      'title': 'Planned',
+      'order': 1,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'inprogress',
+      'itemKind': 'milestone',
+      'title': 'In Progress',
+      'order': 2,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'finished',
+      'itemKind': 'milestone',
+      'title': 'Finished',
+      'order': 3,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'closed',
+      'itemKind': 'milestone',
+      'title': 'Closed',
+      'order': 4,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'todo',
+      'itemKind': 'issue',
+      'title': 'To Do',
+      'order': 1,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'doing',
+      'itemKind': 'issue',
+      'title': 'Doing',
+      'order': 2,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'done',
+      'itemKind': 'issue',
+      'title': 'Done',
+      'order': 3,
+    });
+
+    await db.insertManaged('itemStates', {
+      'key': 'closed',
+      'itemKind': 'issue',
+      'title': 'Closed',
+      'order': 4,
+    });
   }
 }
